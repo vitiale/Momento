69bfd2a00c2fab0a800aeef92f338855673bf861
vitiale <vlirianoluis@gmail.com>
2017-10-19 19:39:21 -0500

    se cambia valor de pa para que de igual a la hoja de calculo ya que esta no esta contemplando la misma formula que la memoria de calculo

--- src/momento/Momento.java
+++ src/momento/Momento.java
@@ -29,17 +29,17 @@ public class Momento {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        double sigma_a = 0.51;
-        double sigma_b = 2.57;
-        double altura_muro = 2.9;
+        double sigma_a = 1.67;
+        double sigma_b = 3.71;
+        double altura_muro = 3.5;
         double P = (sigma_b - sigma_a) / altura_muro;
-        double h1 = 1.90;
-        double h2 = 1.0;
+        double h1 = 3.0;
+        double h2 = 0.5;
         double fs = 1.0;
         double fe = 1.6;
         double xs = altura_muro * 0.6;
-        double pae = 2.995229073;
-        double pa = 2.995229073;
+        double pae = 5.07280459892461;
+        double pa = 3.572916667;
         //****variacion_pae y Ps del excel son lo mismo
         double variacion_pae = pae - pa;
         double factor = 100.0;
@@ -59,6 +59,10 @@ public class Momento {
         double[] vrb = new double[aux];
         double[] vs = new double[aux];
         double[] sum_mcv = new double[aux];
+//        double[] rb = new double[aux];
+//        double[] g1 = new double[aux];
+//        double[] g2 = new double[aux];
+//        double[] g3 = new double[aux];
         for (int i = 0; i < long2.length; i++) {
             long2[i] = altura_muro - (fijo * i);
             long1[i] = (fijo * i);
@@ -66,6 +70,10 @@ public class Momento {
             m1[i] = (sigma_a * long1[i]) * (long1[i] * 0.5);
             m2[i] = ((w2[i] - sigma_a) * long1[i]) / 2 * (1.0 / 3.0) * long1[i];
             mrb[i] = ((long1[i] - altura_muro) > 0) ? -(((sigma_a * Math.pow(altura_muro, 2) * 0.5 * fe) + ((sigma_b - sigma_a) * Math.pow(altura_muro, 2)) * ((1.0 / 6.0) * fe) + (xs * variacion_pae * fs)) / h2) : 0;
+//            g1[i]=(sigma_a * Math.pow(altura_muro, 2) * 0.5 * fe);
+//            g2[i]=((sigma_b - sigma_a) * Math.pow(altura_muro, 2)) * ((1.0 / 6.0) * fe);
+//            g3[i]=(xs * variacion_pae * fs) ;
+//            rb[i]=(((sigma_a * Math.pow(altura_muro, 2) * 0.5 * fe) + ((sigma_b - sigma_a) * Math.pow(altura_muro, 2)) * ((1.0 / 6.0) * fe) + (xs * variacion_pae * fs)) / h2);
             ms[i] = ((long1[i] > (altura_muro - xs)) ? variacion_pae * (long1[i] - (altura_muro - xs)) : 0);
             sum[i] = ((m1[i] + m2[i]) * fe) + (ms[i] * fs) + mrb[i];
             v1[i] = long1[i] * sigma_a;
@@ -74,16 +82,16 @@ public class Momento {
             vs[i] = (long1[i] > (altura_muro - xs)) ? variacion_pae : 0;
             sum_mcv[i] = ((v1[i] + v2[i]) * fe) + (vs[i] * fs) + vrb[i];
         }
-//        for (int j = 0; j < long2.length; j++) {
-//            System.out.println((j + 1) + "                      " + long1[j] + "                      " + long2[j] + "                      " + sigma_a + "                      " + w2[j] + "                      " + m1[j] + "                      " + m2[j] + "                      " + mrb[j] + "                              " + ms[j] + "                           " + sum[j]);
-//            System.out.println("_______________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________");
-//        }
-//        System.out.println("");
-//        System.out.println("");
-//        for (int j = 0; j < v1.length; j++) {
-//            System.out.println((j + 1) + "                      " + v1[j] + "                      " + v2[j] + "                      " + vrb[j] + "                      " + vs[j] + "                      " + sum_mcv[j]);
-//            System.out.println("_______________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________");
-//        }
+        for (int j = 0; j < long2.length; j++) {
+            System.out.println((j + 1) + "                      " + long1[j] + "                      " + long2[j] + "                      " + sigma_a + "                      " + w2[j] + "                      " + m1[j] + "                      " + m2[j] + "                      " + mrb[j] + "                              " + ms[j] + "                           " + sum[j]);
+            System.out.println("_______________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________");
+        }
+        System.out.println("");
+        System.out.println("");
+        for (int j = 0; j < v1.length; j++) {
+            System.out.println((j + 1) + "                      " + v1[j] + "                      " + v2[j] + "                      " + vrb[j] + "                      " + vs[j] + "                      " + sum_mcv[j]);
+            System.out.println("_______________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________");
+        }
         fr=new Frame_momento(sum,long2);
         fr.setVisible(true);
         fc=new Frame_cortante(long2,sum_mcv);
